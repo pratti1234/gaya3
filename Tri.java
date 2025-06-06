e1e841c013f47979c35dcf145404b18672fc4f60
pratti1234 <150414991+pratti1234@users.noreply.github.com>
2025-06-06 15:21:49 +0530

    Add files via upload

---
+++ Tri.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Tri
+{
+public static void main(String args[])
+{
+	Scanner sc=new Scanner(System.in);
+	int a=sc.nextInt();
+	int per=3*a;
+	System.out.println(per);
+	int b=sc.nextInt();
+	double area=0.5*a*b;
+	System.out.println(area);	
+	
+}
+}
